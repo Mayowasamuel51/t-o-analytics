cad89d9a0d30fc690ccc1fdbbfa2c708b3d89f9f
Timi <tunmisebabade@gmail.com>
2023-11-08 15:35:33 +0100

    first commit

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
-const menuBar = document.querySelector(".hamburger")
-const Nav = document.querySelector("nav")
+"use strict"
+const menuBar = document.querySelector(".hamburger");
+const navContainer = document.querySelector("nav>ul");
+const Nav = document.querySelector("nav");
+const links = document.querySelectorAll("nav>ul>li>a")
+const Header = document.getElementsByTagName("header");
 
+const currloc = window.location.pathname;
+
+
+// NAVBAR
 menuBar.addEventListener("click", ()=> {
     if (!Nav.classList.contains("show")) {
         menuBar.innerHTML = `<i class="fa-solid fa-xmark text-2xl"></i>`
@@ -8,5 +16,15 @@ menuBar.addEventListener("click", ()=> {
     }else {
         menuBar.innerHTML = `<i class="fa-solid fa-bars text-2xl"></i>`
         Nav.classList.remove("show")
+    }
+})
+// NAVBAR ONSCROLL
+window.addEventListener("scroll", ()=> {
+    const scrolldis = scrollY;
+    if (scrolldis > 20) {
+        Nav.classList.add("fixed")
+    }
+    else {
+        Nav.classList.remove("fixed")
     }
 })
