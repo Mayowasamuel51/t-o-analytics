6009d1c8d6e67c4c1300b3b9e088dc31fb847805
Timi <tunmisebabade@gmail.com>
2023-11-14 17:21:31 +0100

    adjustments

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         PURPLE: "#c418ac",
         landingBG: "#f4def1",
         textColor: "#d9d9d9",
+        inputColor: "#D9D9D9",
         BLUE: "#2d2065",
         courseText: "#d5d2e0"
       }
