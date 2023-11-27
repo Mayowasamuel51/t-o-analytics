712c02fa231e9db33fc424fadd5b9822d419d11a
Timi <tunmisebabade@gmail.com>
2023-11-27 08:56:38 +0100

    auth

--- src/components/AuthNavBar.jsx
+++ src/components/AuthNavBar.jsx
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion';
 import { Outlet, Link, NavLink, useLocation } from "react-router-dom";
 import { FaBarsStaggered } from "react-icons/fa6";
 import { MdOutlineAddShoppingCart } from "react-icons/md"
+import { FaSearch } from "react-icons/fa";
 import { HiMiniXMark } from "react-icons/hi2";
 // import Footer from './Footer';
 
@@ -19,28 +20,29 @@ const AuthNavBar = ({signout}) => {
         setFixed("show")
     }
     return (
-        <header className={`z-20 fixed w-full left-0 top-0 px-2 py-2 md:px-10 bg-white flex items-center justify-between`}>
+        <header className={`z-20 fixed w-full left-0 top-0 px-2 py-2 md:px-10 bg-white flex items-center gap-10`}>
             <div>
                 <Link to="/">
                     <motion.img initial={{x: -100, opacity: 0}} animate={{x: 0, opacity: 1}} transition={{type:"spring", stiffness: 260, duration: 2000}} src={LOGO} className="md:w-[200px] w-[150px]" alt=""/>
                 </Link>
             </div>
-            <div>
-                <input type="text" name="search" id="search" className='border-2 border-black w-full flex-grow-[3]' />
+            <div className='relative search-box'>
+                <FaSearch className='absolute' />
+                <input type="text" name="search" id="search" className='border-2 border-black w-full h-10 rounded-3xl flex-grow-[3]' />
             </div>
             <nav className={`${fixed} md:relative md:left-0 duration-300 md:top-0 md:w-fit py-5 md:py-0 text-center`}>
                 <ul className="md:flex items-center gap-6 font-normal">
-                    <motion.li whileHover={{scale: 1.2}} transition={{ stiffness:250}} ><NavLink className={({isActive})=> isActive ? " text-black" : "scale-100 hover:text-BLUE"} to="">My Learning</NavLink></motion.li>
-                    <motion.li whileHover={{scale: 1.2}} transition={{ stiffness:250}} ><NavLink className={({isActive})=> isActive ? " text-black" : "scale-100 hover:text-BLUE"} to="">Rules</NavLink></motion.li>
-                    <motion.li whileHover={{scale: 1.2}} transition={{ stiffness:250}} ><NavLink className={({isActive})=> isActive ? " text-black" : "scale-100 hover:text-BLUE"} to="">Mentorship</NavLink></motion.li>
-                    <motion.li whileHover={{scale: 1.2}} transition={{ stiffness:250}} ><NavLink className={({isActive})=> isActive ? " text-black" : "scale-100 hover:text-BLUE"} to="">Link</NavLink></motion.li>
-                    <motion.li whileHover={{scale: 1.2}} transition={{ stiffness:250}} ><NavLink className={({isActive})=> isActive ? " text-black" : "scale-100 hover:text-BLUE"} to="">Payment History</NavLink></motion.li>
+                    <motion.li whileHover={{scale: 1.1}} transition={{ stiffness:250}} ><NavLink className={({isActive})=> isActive ? "text-black" : "scale-100 hover:text-BLUE"} to="">My Learning</NavLink></motion.li>
+                    <motion.li whileHover={{scale: 1.1}} transition={{ stiffness:250}} ><NavLink className={({isActive})=> isActive ? "text-black" : "scale-100 hover:text-BLUE"} to="">Rules</NavLink></motion.li>
+                    <motion.li whileHover={{scale: 1.1}} transition={{ stiffness:250}} ><NavLink className={({isActive})=> isActive ? "text-black" : "scale-100 hover:text-BLUE"} to="">Mentorship</NavLink></motion.li>
+                    <motion.li whileHover={{scale: 1.1}} transition={{ stiffness:250}} ><NavLink className={({isActive})=> isActive ? "text-black" : "scale-100 hover:text-BLUE"} to="">Link</NavLink></motion.li>
+                    <motion.li whileHover={{scale: 1.1}} transition={{ stiffness:250}} ><NavLink className={({isActive})=> isActive ? "text-black" : "scale-100 hover:text-BLUE"} to="">Payment History</NavLink></motion.li>
                     <button onClick={signout} className="my-3 hover:outline-2 hover:outline-offset-2 border-2 border-BLUE hover:bg-transparent hover:text-BLUE duration-300 bg-BLUE text-white px-2 py-1 md:px-3 md:py-2 rounded-md md:rounded-xl font-semibold">
                         Sign Out
                     </button>
                 </ul>
             </nav>
-            <div className="flex items-center gap-3">
+            <div className="flex flex-1 items-center gap-3">
                 <MdOutlineAddShoppingCart size={30} />
                 <div className="flex-1 block md:hidden hamburger">
                     {fixed === "show" ? <HiMiniXMark size={20} onClick={()=> setFixed("")} /> : <FaBarsStaggered size={20} onClick={navBar} />}
