74113f7038c63134a62a836332a6a2c643a57f16
Timi <tunmisebabade@gmail.com>
2023-12-16 01:49:50 +0100

    adjustments

--- src/pages/MyCourses.jsx
+++ src/pages/MyCourses.jsx
@@ -1,7 +1,7 @@
 
 const myCourses = () => {
   return (
-    <div className="min-h-screen flex justify-center items-center">
+    <div className="min-h-screen flex justify-center items-center text-center">
       <h1 className="font-bold text-xl md:text-4xl">YOU HAVE NOT PURCHASED ANY COURSE YET</h1>
     </div>
   )
