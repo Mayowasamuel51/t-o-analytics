e5275b231e8ae0e5cbe4da3407618f9589c04ae9
mayowasamuel51 <fpasamuelmayowa51@gmail.com>
2023-12-26 09:27:24 +0100

    changed

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import ErrorBoundary from './ErrorBoundary';
-import Error from './error';
-import { ContextProvider } from './context/ContextProvider.jsx';
-import { CartItemProvider } from './context/CartItemContext.jsx';
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-import { CookiesProvider } from 'react-cookie';
-const queryClient = new QueryClient()
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import ErrorBoundary from "./ErrorBoundary";
+import Error from "./error";
+import { ContextProvider } from "./context/ContextProvider.jsx";
+import { CartItemProvider } from "./context/CartItemContext.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { CookiesProvider } from "react-cookie";
+const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ErrorBoundary fallback={<Error />}>
-    <CookiesProvider 
-    defaultSetOptions={{ expires: new Date(2029, 11, 26, 12, 30, 0, 0), maxAge:'1000', 
-      path: 'https://www.to-analytics.com/' ,secure:true,  sameSite:"none" , domain:"https://www.to-analytics.com/", httpOnly:true, }} >
-      <CartItemProvider>
-        <ContextProvider>
-          <QueryClientProvider client={queryClient}>
-            <App />
-          </QueryClientProvider>
-        </ContextProvider>
-      </CartItemProvider>
+      <CookiesProvider
+      
+        defaultSetOptions={{
+          expires: new Date(2029, 11, 26, 12, 30, 0, 0),
+          maxAge: "1000",
+          path: "/",
+          secure: true,
+          sameSite: "none",
+          domain: "https://www.to-analytics.com/",
+          httpOnly: true,
+        }}
+      >
+        <CartItemProvider>
+          <ContextProvider>
+            <QueryClientProvider client={queryClient}>
+              <App />
+            </QueryClientProvider>
+          </ContextProvider>
+        </CartItemProvider>
       </CookiesProvider>
     </ErrorBoundary>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
 
 // https://analytics.google.com/analytics/web/#/p416965870/reports/intelligenthome?params=_u..nav%3Dmaui
