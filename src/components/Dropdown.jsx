7a0c0464c3cc5ee8c9b2c84843eb3bdbe5fbb633
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-11-05 07:13:53 +0100

    working show

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -49,7 +49,7 @@ const DashboardDropdown = () => {
             className="block px-4 py-3 hover:bg-gray-100 dark:hover:bg-gray-700 text-gray-800 dark:text-gray-200 transition-all duration-200"
             onClick={() => setOpen(false)}
           >
-            Syllabus (PDF)
+            See Splunk Syllabus
           </a>
         </div>
       )}
