720dc29f6cec60b6dcce8b865bd74842ab8bc8c2
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-11-08 08:24:56 +0100

    https://to-backendapi-v1.onrender.com/

--- src/components/Materials.jsx
+++ src/components/Materials.jsx
@@ -133,9 +133,9 @@ const sampleCourses = [
         ],
       },
 
-      { id: "class5", title: "Class 4 SPL Part 2", videos: [], docs: [] },
+      { id: "class5", title: "Class 4 — SPL Part 2", videos: [], docs: [] },
 
-       { id: "class6", title: "Class 5 Splunk Knowledge Objects", videos: [], docs: [] },
+       { id: "class6", title: "Class 5  — Splunk Knowledge Objects", videos: [], docs: [] },
     ],
   },
 ];
