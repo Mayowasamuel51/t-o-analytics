75074781e33b337b2b502f3106634d772e106063
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-11-12 14:19:19 +0100

    T.O Analytics Splunk Class 4 Quiz

--- src/components/ClassM.jsx
+++ src/components/ClassM.jsx
@@ -31,18 +31,18 @@ const ClassM = () => {
         //   dueDate: "2025-11-10",
         // },
           {
-          id: 4,
-          title: "Splunk class 3 Assignment",
-          url: " https://drive.google.com/file/d/179_DKTqGoGjBrszOPRCJqGu3VFlXUbXe/preview",
-          dueDate: "2025-11-10",
-        },
-        {
            id: 4,
-          title: "Splunk class 3 Assignment",
+          title: "Splunk class 4 Assignment",
           url: "https://docs.google.com/presentation/d/1QH0-bN_phVwY5MGVPOcOnbAC9HmKuShL/preview",
           dueDate: "2025-11-8",
          
         }
+          {
+          id: 5,
+          title: "Splunk class 3 Assignment",
+          url: " https://drive.google.com/file/d/179_DKTqGoGjBrszOPRCJqGu3VFlXUbXe/preview",
+          dueDate: "2025-11-10",
+        },
       ],
      
 
