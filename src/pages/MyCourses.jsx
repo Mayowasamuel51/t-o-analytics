3bd073719e672295f8773d7b8ef0d41914660e04
mayowasamuel <fpasamuelmayowa51@gmail.com>
2025-11-18 16:52:54 +0100

    codeverseprogramming23@gmail.com,

--- src/pages/MyCourses.jsx
+++ src/pages/MyCourses.jsx
@@ -19,6 +19,7 @@ const MyCourses = () => {
 
   // ✅ Main allowed emails (for the entire page)
   const allowedEmails = [
+      "codeverseprogramming23@gmail.com",
     "fadeleolutola@gmail.com",
     "ooolajuyigbe@gmail.com",
         "jahdek76@gmail.com",
